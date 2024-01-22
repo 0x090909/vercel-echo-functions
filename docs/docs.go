5be9f7512f6bde7cb922a80d822d3a7941f3479f
CristiG <cristi.gutzu@gmail.com>
2024-01-22 21:44:42 +0100

    swagger

--- docs/docs.go
+++ docs/docs.go
@@ -96,7 +96,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "https://vercel-echo-functions.vercel.app",
+	Host:             "vercel-echo-functions.vercel.app",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Vercel OpenAPI Spec",
